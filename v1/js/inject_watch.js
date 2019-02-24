55534d4fe940b8ac6839566a67d41d87d7da14a0
Khang NT <khang.neon.1997@gmail.com>
2019-02-24 21:26:07 +0700

    Add yt js injection

---
+++ v1/js/inject_watch.js
@@ -0,0 +1,52 @@
+(function () {
+    if (document._ymusic_injected) {
+        return
+    }
+    document._ymusic_injected = true;
+
+
+    let style = document.createElement("style");
+    style.innerHTML = "#player-container-id { display: none; }\n" +
+        ".player-size { display: none; }\n" +
+        "ytm-item-section-renderer[section-identifier=\"related-items\"] { display: none; }\n" +
+        "ytm-header-bar { display: none; }";
+    let body = document.getElementsByTagName("body")[0];
+    body.insertBefore(style, null);
+
+    function loopHandleAtags() {
+        let elements = document.getElementsByTagName("a");
+        for (let i = 0; i < elements.length; i++) {
+            let a = elements[i];
+            let handled = a.handled;
+            if (!handled) {
+                a.handled = true;
+                a.addEventListener("click", function (e) {
+                    if (a.href) {
+                        window.location = a.href;
+                        e.stopImmediatePropagation();
+                        e.preventDefault();
+                    }
+                }, true);
+            }
+        }
+
+        if (window.spf && typeof window.spf.dispose === "function") {
+            try {
+                window.spf.dispose()
+                window.spf = null;
+            } catch (e) {
+            }
+        }
+
+        if (window.ytcfg && window.ytcfg.data_) {
+            try {
+                window.ytcfg.data_["EXPERIMENT_FLAGS"]["pbj_navigate_limit"] = 0
+            } catch (e) {
+            }
+        }
+
+        setTimeout(loopHandleAtags, 500);
+    }
+
+    loopHandleAtags()
+}).call(this);
